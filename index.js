e47dd5019815c2f034ce7432d9bab9c714df7dda
Brandan Lee <blee2021@208-Engineering-5.local>
2019-05-13 09:02:35 -0700

    work monday

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var bodyParser = require('body-parser')
+const bcrypt = require('bcrypt');
+
+
+//passport
+const session = require("express-session")
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
+app.use(session({secret: "cats"}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 //models
 var Post = require('./models/merchSchema.js')
@@ -11,17 +22,74 @@ app.use(express.static('public'))
 
 //controllers
 var ControllerPosts = require('./controllers/posts.js')
-// var ControllerUsers = require('./controllers/users.js')
+var ControllerUsers = require('./controllers/users.js')
 var ControllerPages = require('./controllers/pages.js')
 
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+
+mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true});
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    console.log('running local strats')
+    User.findOne({ uname: username }, function(err, user) {
+      console.log('in')
+      if (err) {
+        console.log(err)
+        return done(err);
+      }
+      if (user === null){
+        console.log('user not found')
+        return done(null, false, {message: 'Incorrect username.'});
+      }
+      bcrypt.compare(password, user.psw, function(err, result){
+        if (result === false) {
+          console.log('Incorrect Password')
+          return done(null, false, {message: 'Incorrect password.'});
+        }
+        return done(null, user);
+      })
+    });
+  }
+));
+
+app.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login', failureFlash: false }) );
+
+function isLoggedIn(req, res, next){
+  console.log(req.user)
+  if(req.user === undefined){
+    res.redirect('/login')
+    return
+  }
+  next()
+}
 
 //routes
 app.get('/login', ControllerPages.login)
 app.get('/signup', ControllerPages.signup)
 app.get('/', ControllerPages.home)
 app.get('/addmerch', ControllerPosts.addmerch)
-app.get('/tees', ControllerPages.tees)
-app.get('/hoodies', ControllerPages.hoodies)
-app.get('/accessories', ControllerPages.accessories)
+app.get('/tees', isLoggedIn, ControllerPages.tees)
+app.get('/hoodies', isLoggedIn, ControllerPages.hoodies)
+app.get('/accessories', isLoggedIn, ControllerPages.accessories)
+app.post('/signup', ControllerUsers.signedin)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
