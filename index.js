2be1824d742378ef6be25ec559cec3fc01b077b2
Brandan Lee <blee2021@208-Engineering-5.local>
2019-05-15 07:54:52 -0700

    Login

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser')
 const bcrypt = require('bcrypt');
 
